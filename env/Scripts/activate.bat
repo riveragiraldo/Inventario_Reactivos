de588e49c0f04790ec0a034a9d7a6cdeab90d289
Andrés Rivera <47341112+riveragiraldo@users.noreply.github.com>
2023-04-26 08:56:33 -0500

    hj

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\xampp\htdocs\Inventario_Reactivos\env
+set VIRTUAL_ENV=C:\Inventario_Reactivos\env
 
 if not defined PROMPT set PROMPT=$P$G
 
