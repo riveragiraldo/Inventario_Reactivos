c67c5be47b70fdf85cf4f8591404729849438be2
Andrés Rivera <47341112+riveragiraldo@users.noreply.github.com>
2023-08-04 13:09:41 -0500

    Login Redirect

--- reactivos/views.py
+++ reactivos/views.py
@@ -105,7 +105,7 @@ class Index(LoginRequiredMixin, View):  # Utiliza LoginRequiredMixin como clase 
 # acción apropiada según los requisitos del sistema.
 
 
-class CrearUnidades(LoginRequiredMixin, View):
+class CrearUnidades(View,LoginRequiredMixin):
     template_name = 'reactivos/crear_unidades.html'
     groups_required=['Coordinador','Administrador']
 
@@ -117,6 +117,7 @@ class CrearUnidades(LoginRequiredMixin, View):
         context = {
             'usuarios': User.objects.all(),
             'laboratorio': laboratorio,
+            'username': request.user.username,
             }
         for grupo in grupos_usuario:
             
@@ -130,7 +131,7 @@ class CrearUnidades(LoginRequiredMixin, View):
             return render(request, self.template_name, context)
         else:
 
-            messages.error(request,'No tiene permisos para acceder a esta vista')
+            messages.error(request,'No tiene permisos para acceder a la vista Crear Unidades')
             return HttpResponseRedirect(reverse('reactivos:login'))   
 
     def post(self, request, *args, **kwargs):
@@ -2081,93 +2082,93 @@ def estandarizar_nombre(nombre):
 
 
 
-class RedirectURLMixin:
-    next_page = None
-    redirect_field_name = REDIRECT_FIELD_NAME
-    success_url_allowed_hosts = set()
+# class RedirectURLMixin:
+#     next_page = None
+#     redirect_field_name = REDIRECT_FIELD_NAME
+#     success_url_allowed_hosts = set()
 
-    def get_success_url(self):
-        return self.get_redirect_url() or self.get_default_redirect_url()
+#     def get_success_url(self):
+#         return self.get_redirect_url() or self.get_default_redirect_url()
 
-    def get_redirect_url(self):
-        """Return the user-originating redirect URL if it's safe."""
-        redirect_to = self.request.POST.get(
-            self.redirect_field_name, self.request.GET.get(self.redirect_field_name)
-        )
-        url_is_safe = url_has_allowed_host_and_scheme(
-            url=redirect_to,
-            allowed_hosts=self.get_success_url_allowed_hosts(),
-            require_https=self.request.is_secure(),
-        )
-        return redirect_to if url_is_safe else ""
+#     def get_redirect_url(self):
+#         """Return the user-originating redirect URL if it's safe."""
+#         redirect_to = self.request.POST.get(
+#             self.redirect_field_name, self.request.GET.get(self.redirect_field_name)
+#         )
+#         url_is_safe = url_has_allowed_host_and_scheme(
+#             url=redirect_to,
+#             allowed_hosts=self.get_success_url_allowed_hosts(),
+#             require_https=self.request.is_secure(),
+#         )
+#         return redirect_to if url_is_safe else ""
 
-    def get_success_url_allowed_hosts(self):
-        return {self.request.get_host(), *self.success_url_allowed_hosts}
+#     def get_success_url_allowed_hosts(self):
+#         return {self.request.get_host(), *self.success_url_allowed_hosts}
 
-    def get_default_redirect_url(self):
-        """Return the default redirect URL."""
-        if self.next_page:
-            return resolve_url(self.next_page)
-        raise ImproperlyConfigured("No URL to redirect to. Provide a next_page.")
+#     def get_default_redirect_url(self):
+#         """Return the default redirect URL."""
+#         if self.next_page:
+#             return resolve_url(self.next_page)
+#         raise ImproperlyConfigured("No URL to redirect to. Provide a next_page.")
 
-# Vista para el Login
-class LoginView(RedirectURLMixin, FormView):
-    """
-    Display the login form and handle the login action.
-    """
+# # Vista para el Login
+# class LoginView(RedirectURLMixin, FormView):
+#     """
+#     Display the login form and handle the login action.
+#     """
 
-    form_class = AuthenticationForm
-    authentication_form = None
-    template_name = "registration/login.html"
-    redirect_authenticated_user = False
-    extra_context = None
+#     form_class = AuthenticationForm
+#     authentication_form = None
+#     template_name = "registration/login.html"
+#     redirect_authenticated_user = False
+#     extra_context = None
 
-    @method_decorator(sensitive_post_parameters())
-    @method_decorator(csrf_protect)
-    @method_decorator(never_cache)
-    def dispatch(self, request, *args, **kwargs):
-        if self.redirect_authenticated_user and self.request.user.is_authenticated:
-            redirect_to = self.get_success_url()
-            if redirect_to == self.request.path:
-                raise ValueError(
-                    "Redirection loop for authenticated user detected. Check that "
-                    "your LOGIN_REDIRECT_URL doesn't point to a login page."
-                )
-            return HttpResponseRedirect(redirect_to)
-        return super().dispatch(request, *args, **kwargs)
+#     @method_decorator(sensitive_post_parameters())
+#     @method_decorator(csrf_protect)
+#     @method_decorator(never_cache)
+#     def dispatch(self, request, *args, **kwargs):
+#         if self.redirect_authenticated_user and self.request.user.is_authenticated:
+#             redirect_to = self.get_success_url()
+#             if redirect_to == self.request.path:
+#                 raise ValueError(
+#                     "Redirection loop for authenticated user detected. Check that "
+#                     "your LOGIN_REDIRECT_URL doesn't point to a login page."
+#                 )
+#             return HttpResponseRedirect(redirect_to)
+#         return super().dispatch(request, *args, **kwargs)
 
-    def get_default_redirect_url(self):
-        """Return the default redirect URL."""
-        if self.next_page:
-            return resolve_url(self.next_page)
-        else:
-            return resolve_url(settings.LOGIN_REDIRECT_URL)
+#     def get_default_redirect_url(self):
+#         """Return the default redirect URL."""
+#         if self.next_page:
+#             return resolve_url(self.next_page)
+#         else:
+#             return resolve_url(settings.LOGIN_REDIRECT_URL)
 
-    def get_form_class(self):
-        return self.authentication_form or self.form_class
+#     def get_form_class(self):
+#         return self.authentication_form or self.form_class
 
-    def get_form_kwargs(self):
-        kwargs = super().get_form_kwargs()
-        kwargs["request"] = self.request
-        return kwargs
+#     def get_form_kwargs(self):
+#         kwargs = super().get_form_kwargs()
+#         kwargs["request"] = self.request
+#         return kwargs
 
-    def form_valid(self, form):
-        """Security check complete. Log the user in."""
-        auth_login(self.request, form.get_user())
-        return HttpResponseRedirect(self.get_success_url())
+#     def form_valid(self, form):
+#         """Security check complete. Log the user in."""
+#         auth_login(self.request, form.get_user())
+#         return HttpResponseRedirect(self.get_success_url())
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        current_site = get_current_site(self.request)
-        context.update(
-            {
-                self.redirect_field_name: self.get_redirect_url(),
-                "site": current_site,
-                "site_name": current_site.name,
-                **(self.extra_context or {}),
-            }
-        )
-        return context
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         current_site = get_current_site(self.request)
+#         context.update(
+#             {
+#                 self.redirect_field_name: self.get_redirect_url(),
+#                 "site": current_site,
+#                 "site_name": current_site.name,
+#                 **(self.extra_context or {}),
+#             }
+#         )
+#         return context
 
 
 
